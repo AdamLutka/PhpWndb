661567de94c2d1a385765b5e379246482d898546
Adam Lutka <adam.lutka@gmail.com>
2023-02-11 07:10:59 +0100

    Delete all

--- src/Exceptions/IOException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-namespace AL\PhpWndb\Exceptions;
-
-class IOException extends \RuntimeException
-{
-
-}
--- src/Model/Exceptions/SynsetCreateException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace AL\PhpWndb\Model\Exceptions;
-
-class SynsetCreateException extends \RuntimeException
-{
-	
-}
